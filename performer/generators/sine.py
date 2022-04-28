e9d63e59956fa049d4547e1d77ee1a9c3a97cd39
Torque (Tareq) El Dandachi <tareqdandachi@gmail.com>
2022-04-28 13:58:39 -0400

    why...

--- performer/generators/sine.py
+++ performer/generators/sine.py
@@ -3,8 +3,6 @@ import numpy as np
 from .generator import Generator
 
 class Sin(Generator):
-    # TODO: frequency
-
     programmed_fs = 44100
 
     SIN = np.sin(2*np.pi* np.arange(0, programmed_fs*2)/programmed_fs)
@@ -15,12 +13,10 @@ class Sin(Generator):
     def generate(self, t, fs):
 
         if fs!=self.programmed_fs:
-            SIN = np.sin(2*np.pi* np.arange(0, programmed_fs*2)/programmed_fs)
+            self.SIN = np.sin(2*np.pi* np.arange(0, self.programmed_fs*2)/self.programmed_fs)
             self.programmed_fs = fs
 
         # return np.sin(2*np.pi* t /fs)
-
-        #TODO: try precomputing!!
 
         return self.SIN[t.astype(int) % fs*2]
 
